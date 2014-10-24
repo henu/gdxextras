617d1ed0597cde072286b63d003a40154657fd77
Henrik Heino <henrik.heino@gmail.com>
2014-10-25 00:24:14 +0300

    Styling principles of Button changed. It now has own class for this.

--- gui/Button.java
+++ gui/Button.java
@@ -1,10 +1,9 @@
 package fi.henu.gdxextras.gui;
 
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
 import com.badlogic.gdx.math.Vector2;
+
 import fi.henu.gdxextras.Font;
 
 // TODO: Make toggling on/off available!
@@ -12,32 +11,17 @@ public class Button extends Widget
 {
 
 	// This is used to set default background and selected graphics for Button
-	public static void setDefaultGraphics(AtlasRegion region_left, AtlasRegion region_right, Texture tex_center, AtlasRegion region_hilight_left, AtlasRegion region_hilight_right, Texture tex_hilight_center, float end_width, float height)
+	public static void setDefaultStyle(ButtonStyle style)
 	{
-		default_region_left = region_left;
-		default_region_right = region_right;
-		default_tex_center = tex_center;
-		default_region_hilight_left = region_hilight_left;
-		default_region_hilight_right = region_hilight_right;
-		default_tex_hilight_center = tex_hilight_center;
-		default_end_width = end_width;
-		default_height = height;
+		default_style = style;
 	}
 
-	public Button(float height, Color bg_color)
+	public Button()
 	{
 		super();
-		// Set appearance options
-		this.height = height;
-		this.bg_color = bg_color;
-		this.bg_color_disabled = getDisabledColor(bg_color);
 		// Reset icon and label
 		icon = null;
-		icon_color = null;
-		icon_color_disabled = null;
 		label = null;
-		label_color = null;
-		label_color_disabled = null;
 		label_pixelheight = 0;
 		label_font = null;
 		// Reset state
@@ -46,21 +30,17 @@ public class Button extends Widget
 		enabled = true;
 	}
 
-	public void setLabel(String label, float pixelheight, Color color, Font font)
+	public void setLabel(String label, float pixelheight, Font font)
 	{
 		this.label = label;
 		label_pixelheight = pixelheight;
-		label_color = color;
-		this.label_color_disabled = getDisabledColor(label_color);
 		label_font = font;
 		markToNeedReposition();
 	}
 	
-	public void setIcon(AtlasRegion icon, Color icon_color)
+	public void setIcon(AtlasRegion icon)
 	{
 		this.icon = icon;
-		this.icon_color = icon_color;
-		this.icon_color_disabled = getDisabledColor(icon_color);
 	}
 
 	// Hilight can be used to display constant selection of button.
@@ -118,41 +98,41 @@ public class Button extends Widget
 	protected void doRendering(SpriteBatch batch)
 	{
 		if (!enabled) {
-			batch.setColor(bg_color_disabled);
+			batch.setColor(getStyle().bg_color_disabled);
 		} else {
-			batch.setColor(bg_color);
+			batch.setColor(getStyle().bg_color);
 		}
-		float scale = height / default_height;
+		float scale = getStyle().scaling;
 		// Render background
-		renderHorizontalBar(batch, default_region_left, default_end_width, default_region_right, default_end_width, default_tex_center, default_height, getPositionX(), getPositionY(), getWidth(), getHeight(), scale);
+		renderHorizontalBar(batch, getStyle().region_left, getStyle().side_padding, getStyle().region_right, getStyle().side_padding, getStyle().tex_center, getStyle().height, getPositionX(), getPositionY(), getWidth(), getHeight(), scale);
 		// Render possible hilights
 		if (pressed && enabled) {
 			batch.setColor(1, 1, 1, 1);
-			renderHorizontalBar(batch, default_region_hilight_left, default_end_width, default_region_hilight_right, default_end_width, default_tex_hilight_center, default_height, getPositionX(), getPositionY(), getWidth(), getHeight(), scale);
+			renderHorizontalBar(batch, getStyle().region_hilight_left, getStyle().side_padding, getStyle().region_hilight_right, getStyle().side_padding, getStyle().tex_hilight_center, getStyle().height, getPositionX(), getPositionY(), getWidth(), getHeight(), scale);
 		}
 		if (hilight) {
 			batch.setColor(1, 1, 1, 1);
-			renderHorizontalBar(batch, default_region_hilight_left, default_end_width, default_region_hilight_right, default_end_width, default_tex_hilight_center, default_height, getPositionX(), getPositionY(), getWidth(), getHeight(), scale);
+			renderHorizontalBar(batch, getStyle().region_hilight_left, getStyle().side_padding, getStyle().region_hilight_right, getStyle().side_padding, getStyle().tex_hilight_center, getStyle().height, getPositionX(), getPositionY(), getWidth(), getHeight(), scale);
 		}
 		// Render possible icon
 		if (icon != null) {
 			if (!enabled) {
-				batch.setColor(icon_color_disabled);
+				batch.setColor(getStyle().icon_color_disabled);
 			} else {
-				batch.setColor(icon_color);
+				batch.setColor(getStyle().icon_color);
 			}			
-			renderFromCenter(batch, icon, getPositionX() + default_end_width * scale, getCenterY(), scale);
+			renderFromCenter(batch, icon, getPositionX() + getStyle().side_padding * scale, getCenterY(), scale);
 		}
 		// Render possible label
 		if (label != null) {
-			float draw_x = getPositionX() + default_end_width * scale;
+			float draw_x = getPositionX() + getStyle().side_padding * scale;
 			if (icon != null) {
-				draw_x += default_end_width * scale;
+				draw_x += getStyle().side_padding * scale;
 			}
 			if (!enabled) {
-				label_font.renderString(batch, label, draw_x, getPositionY() + (default_height / 2) * scale + label_pixelheight / 2, label_pixelheight, label_color_disabled);
+				label_font.renderString(batch, label, draw_x, getPositionY() + (getStyle().height / 2) * scale + label_pixelheight / 2, label_pixelheight, getStyle().label_color_disabled);
 			} else {
-				label_font.renderString(batch, label, draw_x, getPositionY() + (default_height / 2) * scale + label_pixelheight / 2, label_pixelheight, label_color);
+				label_font.renderString(batch, label, draw_x, getPositionY() + (getStyle().height / 2) * scale + label_pixelheight / 2, label_pixelheight, getStyle().label_color);
 			}			
 		}
 		batch.setColor(1, 1, 1, 1);
@@ -160,12 +140,11 @@ public class Button extends Widget
 
 	protected float doGetMinWidth()
 	{
-		float scale = height / default_height;
-		float min_width = default_end_width * 2f * scale;
+		float min_width = getStyle().side_padding * 2f * getStyle().scaling;
 		if (label != null) {
 			min_width += label_font.getStringWidth(label, label_pixelheight);
 			if (icon != null) {
-				min_width += default_end_width * scale;
+				min_width += getStyle().side_padding * getStyle().scaling;
 			}
 		}
 		return min_width;
@@ -173,29 +152,14 @@ public class Button extends Widget
 
 	protected float doGetMinHeight(float width)
 	{
-		return height;
+		return getStyle().height * getStyle().scaling;
 	}
 
-	// Default graphics for all buttons
-	private static AtlasRegion default_region_left = null;
-	private static AtlasRegion default_region_right = null;
-	private static Texture default_tex_center = null;
-	private static AtlasRegion default_region_hilight_left = null;
-	private static AtlasRegion default_region_hilight_right = null;
-	private static Texture default_tex_hilight_center = null;
-	private static float default_end_width = 0;
-	private static float default_height = 0;
+	private static ButtonStyle default_style;
 
 	// Options
-	private float height;
-	private Color bg_color;
-	private Color bg_color_disabled;
 	private AtlasRegion icon;
-	private Color icon_color;
-	private Color icon_color_disabled;
 	private String label;
-	private Color label_color;
-	private Color label_color_disabled;
 	private float label_pixelheight;
 	private Font label_font;
 
@@ -204,4 +168,8 @@ public class Button extends Widget
 	private boolean pressed;
 	private boolean enabled;
 
+	private ButtonStyle getStyle()
+	{
+		return default_style;
+	}
 }
---
+++ gui/ButtonStyle.java
@@ -0,0 +1,24 @@
+package fi.henu.gdxextras.gui;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
+
+public class ButtonStyle
+{
+	public AtlasRegion region_left;
+	public AtlasRegion region_right;
+	public Texture tex_center;
+	public AtlasRegion region_hilight_left;
+	public AtlasRegion region_hilight_right;
+	public Texture tex_hilight_center;
+	public float side_padding = 0f;
+	public float height = 0f;
+	public float scaling = 1f;
+	public Color bg_color = Color.WHITE;
+	public Color bg_color_disabled = Color.GRAY;
+	public Color label_color = Color.BLACK;
+	public Color label_color_disabled = Color.DARK_GRAY;
+	public Color icon_color;
+	public Color icon_color_disabled;
+}
