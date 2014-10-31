6838840c8812ac1caf47b6abc40913cf0c7d49df
Henrik Heino <henrik.heino@gmail.com>
2014-10-31 21:17:36 +1100

    Label now has class to describe its style.

--- gui/Label.java
+++ gui/Label.java
@@ -1,39 +1,33 @@
 package fi.henu.gdxextras.gui;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.BitmapFont.TextBounds;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
-import fi.henu.gdxextras.Font;
 
 public class Label extends Widget
 {
 
-	public Label(float height, Font font)
+	public static void setDefaultStyle(LabelStyle style)
+	{
+		default_style = style;
+	}
+
+	public Label()
 	{
 		super();
 		text = "";
-		text_total_width = 0;
-		this.height = height;
-		this.font = font;
-		color = new Color(1, 1, 1, 1);
 		text_align = Alignment.LEFT;
-		multiline = null;
-		shadow = null;
+		multiline = false;
 	}
 
 	public void setText(String text)
 	{
 		this.text = text;
-		text_total_width = font.getStringWidth(text, height);
 		markToNeedReposition();
 	}
 	
-	public void setColor(Color color)
-	{
-		this.color.set(color);
-	}
-
 	public void setTextAlignment(Alignment align)
 	{
 		text_align = align;
@@ -41,187 +35,221 @@ public class Label extends Widget
 	
 	public void setMultiline(boolean multiline)
 	{
-		if (multiline) {
-			if (this.multiline == null) {
-				this.multiline = new Array<String>();
-				markToNeedReposition();
-			}
-		} else {
-			if (this.multiline != null) {
-				this.multiline = null;
-				markToNeedReposition();
-			}
+		if (this.multiline != multiline) {
+			markToNeedReposition();
 		}
+		this.multiline = multiline;
 	}
 	
-	// Give null to disable shadow
-	public void setShadow(Vector2 shadow)
-	{
-		if (shadow == null) {
-			this.shadow = null;
-		} else {
-			this.shadow = new Vector2(shadow);
-		}
-	}
-
 	protected void doRendering(SpriteBatch batch)
 	{
-		if (multiline == null) {
-			if (shadow != null) {
-				renderLineWithAlignment(batch, text, text_total_width, getPositionX() + shadow.x, getPositionY() + shadow.y, getWidth(), Color.BLACK);
+		// Get font and scale it correctly
+		BitmapFont font = getStyle().font;
+		font.setScale(1);
+		float lineheight = 	font.getLineHeight();
+		font.setScale(getStyle().height / lineheight);
+
+		if (!multiline) {
+			if (getStyle().shadow != null) {
+				renderLineWithAlignment(batch, text, font, getPositionX() + getStyle().shadow.x, getPositionY() + getStyle().shadow.y, getWidth(), Color.BLACK);
 			}
-			renderLineWithAlignment(batch, text, text_total_width, getPositionX(), getPositionY(), getWidth(), color);
+			renderLineWithAlignment(batch, text, font, getPositionX(), getPositionY(), getWidth(), getStyle().color);
 		} else {
-			if (shadow != null) {
-				float render_pos_y = getPositionY() + shadow.y;
-				for (int line_id = multiline.size - 1; line_id >= 0; line_id --) {
-					String line = multiline.get(line_id);
-					float line_width = multiline_widths[line_id];
-					renderLineWithAlignment(batch, line, line_width, getPositionX() + shadow.x, render_pos_y, getWidth(), Color.BLACK);
-					render_pos_y += height;
+			// Read all lines first
+			Array<String> lines = new Array<String>(true, 0, String.class);
+			int text_ofs = 0;
+			while (text_ofs < text.length()) {
+				// Read one line and update counters
+				String line = extractLine(text, text_ofs, font, getWidth());
+				text_ofs += line.length();
+				lines.add(line);
+				// Skip whitespace after the line
+				while (text_ofs < text.length() && (text.charAt(text_ofs) == ' ' || text.charAt(text_ofs) == '\t' || text.charAt(text_ofs) == '\n')) {
+					boolean is_endline = (text.charAt(text_ofs) == '\n'); 
+					++ text_ofs;
+					if (is_endline) {
+						break;
+					}
 				}
 			}
-			float render_pos_y = getPositionY();
-			for (int line_id = multiline.size - 1; line_id >= 0; line_id --) {
-				String line = multiline.get(line_id);
-				float line_width = multiline_widths[line_id];
-				renderLineWithAlignment(batch, line, line_width, getPositionX(), render_pos_y, getWidth(), color);
-				render_pos_y += height;
+			// Now render lines
+			float draw_y = getPositionY() + (lines.size - 1) * getStyle().height;
+			for (int line = 0; line < lines.size; ++ line) {
+				if (getStyle().shadow != null) {
+					renderLineWithAlignment(batch, lines.get(line), font, getPositionX() + getStyle().shadow.x, draw_y + getStyle().shadow.y, getWidth(), Color.BLACK);
+				}
+				renderLineWithAlignment(batch, lines.get(line), font, getPositionX(), draw_y, getWidth(), getStyle().color);
+				draw_y -= getStyle().height;
 			}
 		}
 	}
 
 	protected void doRepositioning()
 	{
-		// Only multiline support needs something to be done
-		if (multiline != null) {
-			formMultilines(multiline, getWidth(), true);
-		}
 	}
 
 	protected float doGetMinWidth()
 	{
-		if (multiline == null) {
-			return text_total_width;
+		// Get font and scale it correctly
+		BitmapFont font = getStyle().font;
+		font.setScale(1);
+		float lineheight = 	font.getLineHeight();
+		font.setScale(getStyle().height / lineheight);
+
+		// Calculate minimum width
+		if (!multiline) {
+			TextBounds bounds = font.getBounds(text);
+			return bounds.width;
 		} else {
 			float result = 0;
 			String word = "";
 			for (int text_idx = 0; text_idx < text.length(); text_idx ++) {
 				char c = text.charAt(text_idx);
-				if (c == ' ' || c == '\n') {
-					result = Math.max(result, font.getStringWidth(word, height));
+				if (c == ' ' || c == '\t' || c == '\n') {
+					TextBounds bounds = font.getBounds(word);
+					result = Math.max(result, bounds.width);
 					word = "";
 				} else {
 					word += c;
 				}
 			}
-			result = Math.max(result, font.getStringWidth(word, height));
+			TextBounds bounds = font.getBounds(word);
+			result = Math.max(result, bounds.width);
 			return result;
 		}
 	}
 
 	protected float doGetMinHeight(float width)
 	{
-		if (multiline == null) {
-			return height;
+		if (!multiline) {
+			return getStyle().height;
 		} else {
-			Array<String> temp_lines = new Array<String>();
-			formMultilines(temp_lines, width, false);
-			return temp_lines.size * height;
+			// Get font and scale it correctly
+			BitmapFont font = getStyle().font;
+			font.setScale(1);
+			float lineheight = 	font.getLineHeight();
+			font.setScale(getStyle().height / lineheight);
+			
+			int text_ofs = 0;
+			int lines = 0;
+			while (text_ofs < text.length()) {
+				// Read one line and update counters
+				String line = extractLine(text, text_ofs, font, width);
+				text_ofs += line.length();
+				++ lines;
+				// Skip whitespace after the line
+				while (text_ofs < text.length() && (text.charAt(text_ofs) == ' ' || text.charAt(text_ofs) == '\t' || text.charAt(text_ofs) == '\n')) {
+					boolean is_endline = (text.charAt(text_ofs) == '\n'); 
+					++ text_ofs;
+					if (is_endline) {
+						break;
+					}
+				}
+			}
+			return lines * getStyle().height;
 		}
 	}
-	
+
+	private static LabelStyle default_style;
+
 	private String text;
-	private float text_total_width;
-	private float height;
-	private Font font;
-	private Color color;
 	private Alignment text_align;
-	private Vector2 shadow = null;
 
 	// This also tells if multiple lines are supported. Null means not.
-	private Array<String> multiline;
-	
-	private float[] multiline_widths = null;
+	private boolean multiline;
 
-	private void renderLineWithAlignment(SpriteBatch batch, String text, float text_width, float pos_x, float pos_y, float width, Color color)
+	private void renderLineWithAlignment(SpriteBatch batch, String text, BitmapFont font, float pos_x, float pos_y, float width, Color color)
 	{
+		// Set color
+		font.setColor(color);
+
+		float text_width = font.getBounds(text).width;
 		if (text_align == Alignment.LEFT) {
-			font.renderString(batch, text, pos_x, pos_y + height, height, color);
+			font.draw(batch, text, pos_x, pos_y + getStyle().height);
 		} else {
 			float extra_space = width - text_width;
 			if (text_align == Alignment.RIGHT) {
-				font.renderString(batch, text, pos_x + extra_space, pos_y + height, height, color);
+				font.draw(batch, text, pos_x + extra_space, pos_y + getStyle().height);
 			} else {
-				font.renderString(batch, text, pos_x + extra_space / 2, pos_y + height, height, color);
+				font.draw(batch, text, pos_x + extra_space / 2, pos_y + getStyle().height);
 			}
 		}
 	}
 
-	private void formMultilines(Array<String> lines, float width, boolean form_multiline_widths)
+	// Extracts one line that fits to given width. Whitespace after the
+	// line is not included, not even the possible endline. If first
+	// word does not fit to line, then substring of it is returned.
+	private static String extractLine(String text, int text_ofs, BitmapFont font, float width)
 	{
-		lines.clear();
-		
 		String line = "";
 		String word = "";
-		for (int text_idx = 0; text_idx < text.length(); text_idx ++) {
-			char c = text.charAt(text_idx);
-			if (c == ' ' || c == '\n') {
-				// If this is first word, then add it to line, no matter what
-				if (line.length() == 0) {
-					line = word + " ";
-				}
-				// If there is already stuff at current line, then
-				// we have to check if this word fits there. 
-				else {
-					float line_width = font.getStringWidth(line + word, height);
-					if (line_width > width && word.length() > 0) {
-						lines.add(line);
-						line = word + " ";
-					} else {
-						line += word + " ";
+		String whitespace = "";
+		while (true) {
+			char c;
+			if (text_ofs < text.length()) {
+				c = text.charAt(text_ofs);
+			} else {
+				c = '\0';
+			}
+			// Whitespace or end of input
+			if (c == ' ' || c == '\t' || c == '\n' || text_ofs >= text.length()) {
+				// If there is word
+				if (word.length() > 0) {
+					// If word fits to the line
+					if (font.getBounds(line + whitespace + word).width <= width) {
+						line += whitespace + word;
 					}
+					// If word does not fit and it's the only word
+					else if (line.length() == 0) {
+						// Word is so long, that it does not fit to one line. We
+						// need to cut it. Use binary search to find good length
+						// quickly. Also include possible whitespace to the line.
+						line = whitespace + word;
+						int good_length_min = 0;
+						int good_length_max = line.length();
+						// "good_length_min" might be the correct answer,
+						// while "good_length_max" is always too long.
+						while (good_length_max - good_length_min > 1) {
+							int good_length_half = (good_length_min + good_length_max) / 2;
+							String good_length_half_substr = line.substring(0, good_length_half);
+							float width_now = font.getBounds(good_length_half_substr).width;
+							if (width_now == width) {
+								return good_length_half_substr;
+							} else if (width_now < width) {
+								good_length_min = good_length_half;
+							} else {
+								good_length_max = good_length_half;
+							}
+						}
+						// Ensure at least first letter of word is got
+						good_length_min = Math.max(good_length_min, 1);
+						return line.substring(0, good_length_min);
+					}
+					// Word does not fit, and there is
+					// already other stuff on the line
+					else {
+						return line;
+					}
+					whitespace = "";
+					word = "";
 				}
-				word = "";
-				// If this was endline, then add new line
-				if (c == '\n') {
-					lines.add(line);
-					line = "";
+				
+				// If whitespace was line break or if end was
+				// reached, then stop and return current line
+				if (c == '\n' || text_ofs >= text.length()) {
+					return line;
 				}
+				
+				whitespace += c;
 			} else {
 				word += c;
 			}
-		}
-		if (word.length() > 0) {
-			// If this is first word, then add it to line, no matter what
-			if (line.length() == 0) {
-				lines.add(word);
-			}
-			// If there is already stuff at current line, then
-			// we have to check if this word fits there. 
-			else {
-				float line_width = font.getStringWidth(line + word, height);
-				if (line_width > width) {
-					lines.add(line);
-					lines.add(word);
-				} else {
-					lines.add(line + word);
-				}
-			}
-		}
-		
-		// Go lines through and remove whitespace from their tails.
-		for (int line_id = 0; line_id < lines.size; line_id ++) {
-			lines.set(line_id, lines.get(line_id).replaceAll("[\\s]+$", ""));
-		}
-		
-		// Form widths, if requested
-		if (form_multiline_widths) {
-			multiline_widths = new float[lines.size];
-			for (int line_id = 0; line_id < lines.size; line_id ++) {
-				multiline_widths[line_id] = font.getStringWidth(lines.get(line_id), height);
-			}
+			++ text_ofs;
 		}
 	}
 
+	private LabelStyle getStyle()
+	{
+		return default_style;
+	}
 }
---
+++ gui/LabelStyle.java
@@ -0,0 +1,15 @@
+package fi.henu.gdxextras.gui;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.math.Vector2;
+
+public class LabelStyle
+{
+
+	public float height;
+	public BitmapFont font;
+	public Color color;
+	public Vector2 shadow;
+
+}
