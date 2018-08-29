44995c6bf6b92c2deb63a18a72ebd9235a1fab41
Henrik Heino <henrik.heino@gmail.com>
2018-08-30 00:54:11 +0300

    RingBuffer: Bugfix and .size()

--- containers/RingBuffer.java
+++ containers/RingBuffer.java
@@ -4,78 +4,83 @@ import java.nio.BufferUnderflowException;
 
 public class RingBuffer<T>
 {
-	public void add(T obj)
+	public void add(T item)
 	{
-		ensureSpace(size + 1);
-		items[write] = obj;
-		write = (write + 1) % items.length;
-		size += 1;
+		ensureSpace(items + 1);
+		arr[write] = item;
+		write = (write + 1) % arr.length;
+		items += 1;
 	}
 
 	public T pop()
 	{
-		if (size <= 0) {
+		if (items <= 0) {
 			throw new BufferUnderflowException();
 		}
-		T result = items[read ++];
-		read = (read + 1) % items.length;
-		-- size;
-		ensureSpace(size);
+		T result = arr[read];
+		read = (read + 1) % arr.length;
+		--items;
+		ensureSpace(items);
 		return result;
+	}
+
+	public int size()
+	{
+		return items;
 	}
 
 	public boolean empty()
 	{
-		return size == 0;
+		return items == 0;
 	}
 
-	private T[] items;
+	private T[] arr;
 
 	private int read;
 	private int write;
-	private int size;
+	private int items;
 
 	private void ensureSpace(int new_size)
 	{
 		// Initial allocation
-		if (items == null) {
+		if (arr == null) {
 			if (new_size > 0) {
-				items = (T[])new Object[new_size];
+				arr = (T[])new Object[new_size];
 			}
 			return;
 		}
 
 		// If there is too much space
-		if (new_size * 4 < items.length) {
+		if (new_size * 4 < arr.length) {
 // TODO: Code this!
 			return;
 		}
 
 		// If there is enough space already
-		if (new_size <= items.length) {
+		if (new_size <= arr.length) {
 			return;
 		}
 
 		// If bigger space is needed
-		new_size = Math.max(new_size, items.length * 2);
-		T[] new_items = (T[])new Object[new_size];
+		new_size = Math.max(new_size, arr.length * 2);
+		T[] new_arr = (T[])new Object[new_size];
 
 		// Copy items so they are at the beginning of new items
-		if (size > 0) {
+		if (items > 0) {
 			if (read < write) {
-				System.arraycopy(items, read, new_items, 0, size);
+				System.arraycopy(arr, read, new_arr, 0, items);
 			} else {
-				int first_part_length = items.length - read;
-				System.arraycopy(items, read, new_items, 0, first_part_length);
-				int second_part_length = size - first_part_length;
+				int first_part_length = arr.length - read;
+				System.arraycopy(arr, read, new_arr, 0, first_part_length);
+				int second_part_length = items - first_part_length;
 				if (second_part_length > 0) {
-					System.arraycopy(items, 0, new_items, first_part_length, second_part_length);
+					System.arraycopy(arr, 0, new_arr, first_part_length, second_part_length);
 				}
 			}
 		}
 
 		read = 0;
-		write = size;
-		items = new_items;
+		write = items;
+		arr = new_arr;
 	}
 }
