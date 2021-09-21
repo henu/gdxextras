7dc04ca4a0f047f7ee3fdf049e313f379a99bb04
Henrik Heino <henrik.heino@gmail.com>
2021-09-21 03:20:27 +0300

    Hash.PDKDF2SHA256()

--- crypto/Hash.java
+++ crypto/Hash.java
@@ -2,12 +2,17 @@ package fi.henu.gdxextras.crypto;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 
 public class Hash
 {
 	public static byte[] SHA256(byte[] data)
 	{
-		MessageDigest digest = null;
+		MessageDigest digest;
 		try {
 			digest = MessageDigest.getInstance("SHA-256");
 		}
@@ -15,5 +20,18 @@ public class Hash
 			throw new RuntimeException("Unable to initialize SHA256!");
 		}
 		return digest.digest(data);
+	}
+
+	public static byte[] PDKDF2SHA256(int iterations, String salt, String data)
+	{
+		KeySpec spec = new PBEKeySpec(data.toCharArray(), salt.getBytes(), iterations, 256);
+		try {
+			SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+			return factory.generateSecret(spec).getEncoded();
+		} catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException("Unable to initialize PDKF2 SHA256!");
+		} catch (InvalidKeySpecException e) {
+			throw new RuntimeException("Invalid key!");
+		}
 	}
 }
