743512458920f5be5c1c238ddf4db923d824d1a8
Henrik Heino <henrik.heino@gmail.com>
2023-02-14 05:23:22 +0200

    New class VirtualKeyboardPlaceholder

---
+++ gui/VirtualKeyboardPlaceholder.java
@@ -0,0 +1,36 @@
+package fi.henu.gdxextras.gui;
+
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Gdx;
+
+// TODO: It would be nice if this Widget could automatically detect if the keyboard is open!
+public class VirtualKeyboardPlaceholder extends Widget
+{
+	public void setKeyboardVisible(boolean visible)
+	{
+		if (visible != keyboard_visible) {
+			markToNeedReposition();
+		}
+		keyboard_visible = visible;
+	}
+
+	@Override
+	protected float doGetMinWidth()
+	{
+		return 0;
+	}
+
+	@Override
+	protected float doGetMinHeight(float width)
+	{
+		if (Gdx.app.getType() != Application.ApplicationType.Android && Gdx.app.getType() != Application.ApplicationType.iOS) {
+			return 0;
+		}
+		if (keyboard_visible) {
+			return Gdx.graphics.getHeight() / 2.0f;
+		}
+		return 0;
+	}
+
+	private boolean keyboard_visible = false;
+}
