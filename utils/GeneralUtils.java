760ef7f3808d96f9f16b82c20e534d746bd930f2
Henrik Heino <henrik.heino@gmail.com>
2026-01-22 21:23:24 +0200

    GeneralUtils.KbMetaKeys() for checking meta keys

--- utils/GeneralUtils.java
+++ utils/GeneralUtils.java
@@ -1,9 +1,21 @@
 package fi.henu.gdxextras.utils;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+
 import java.util.concurrent.Callable;
 
 public class GeneralUtils
 {
+	public static final int KB_META_ALT_LEFT = 0x0001;
+	public static final int KB_META_ALT_RIGHT = 0x0002;
+	public static final int KB_META_ALT = 0x0004;
+	public static final int KB_META_CONTROL_LEFT = 0x0008;
+	public static final int KB_META_CONTROL_RIGHT = 0x0010;
+	public static final int KB_META_CONTROL = 0x0020;
+	public static final int KB_META_SHIFT_LEFT = 0x0040;
+	public static final int KB_META_SHIFT_RIGHT = 0x0080;
+	public static final int KB_META_SHIFT = 0x0100;
 
 	public static <T> T retry(Callable<T> func, int max_tries, long sleep_ms)
 	{
@@ -27,4 +39,63 @@ public class GeneralUtils
 		}
 	}
 
+	// Checks if a set of meta keys is pressed at the same time
+	public static boolean KbMetaKeys(int meta_key_flags)
+	{
+		// Check alt keys
+		if ((meta_key_flags & KB_META_ALT_LEFT) != 0) {
+			if (!Gdx.input.isKeyPressed(Input.Keys.ALT_LEFT)) return false;
+		}
+		if ((meta_key_flags & KB_META_ALT_RIGHT) != 0) {
+			if (!Gdx.input.isKeyPressed(Input.Keys.ALT_RIGHT)) return false;
+		}
+		if ((meta_key_flags & KB_META_ALT) != 0) {
+			if (!Gdx.input.isKeyPressed(Input.Keys.ALT_LEFT) && !Gdx.input.isKeyPressed(Input.Keys.ALT_RIGHT)) return false;
+		} else {
+			if ((meta_key_flags & KB_META_ALT_LEFT) == 0) {
+				if (Gdx.input.isKeyPressed(Input.Keys.ALT_LEFT)) return false;
+			}
+			if ((meta_key_flags & KB_META_ALT_RIGHT) == 0) {
+				if (Gdx.input.isKeyPressed(Input.Keys.ALT_RIGHT)) return false;
+			}
+		}
+
+		// Check control keys
+		if ((meta_key_flags & KB_META_CONTROL_LEFT) != 0) {
+			if (!Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT)) return false;
+		}
+		if ((meta_key_flags & KB_META_CONTROL_RIGHT) != 0) {
+			if (!Gdx.input.isKeyPressed(Input.Keys.CONTROL_RIGHT)) return false;
+		}
+		if ((meta_key_flags & KB_META_CONTROL) != 0) {
+			if (!Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT) && !Gdx.input.isKeyPressed(Input.Keys.CONTROL_RIGHT)) return false;
+		} else {
+			if ((meta_key_flags & KB_META_CONTROL_LEFT) == 0) {
+				if (Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT)) return false;
+			}
+			if ((meta_key_flags & KB_META_CONTROL_RIGHT) == 0) {
+				if (Gdx.input.isKeyPressed(Input.Keys.CONTROL_RIGHT)) return false;
+			}
+		}
+
+		// Check alt keys
+		if ((meta_key_flags & KB_META_SHIFT_LEFT) != 0) {
+			if (!Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT)) return false;
+		}
+		if ((meta_key_flags & KB_META_SHIFT_RIGHT) != 0) {
+			if (!Gdx.input.isKeyPressed(Input.Keys.SHIFT_RIGHT)) return false;
+		}
+		if ((meta_key_flags & KB_META_SHIFT) != 0) {
+			if (!Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT) && !Gdx.input.isKeyPressed(Input.Keys.SHIFT_RIGHT)) return false;
+		} else {
+			if ((meta_key_flags & KB_META_SHIFT_LEFT) == 0) {
+				if (Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT)) return false;
+			}
+			if ((meta_key_flags & KB_META_SHIFT_RIGHT) == 0) {
+				if (Gdx.input.isKeyPressed(Input.Keys.SHIFT_RIGHT)) return false;
+			}
+		}
+
+		return true;
+	}
 }
